183304436e7c30002ace844063c30491b82d8932
marcel barrios <marcelbarrios1994@gmail.com>
2017-07-25 20:03:15 -0400

    first commit

---
+++ Exercises/1.2_Basic_Operations.js
@@ -0,0 +1,11 @@
+// Video 1.1 accompanies this exercise
+
+// In the console, enter any expression which uses
+// +, -, *, or /, and whose answer evaluates to 36.
+
+console.log('Hello World');
+
+console.log(17 % 8);
+console.log(7 * 7);
+
+console.log(9*4);
---
+++ Exercises/2.5_Variable_3.js
@@ -0,0 +1,10 @@
+
+
+// Video 2.4 accompanies this exercise
+
+// In the console, declare a variable called firstName and
+// store your first name inside it. Then log firstName to the console.
+
+const firstName = "Marcel"
+
+console.log(firstName);
